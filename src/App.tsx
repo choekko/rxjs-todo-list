94b6950d8b42aa217e4a167e506961ab093cd19a
choekko <choekko@gmail.com>
2022-04-24 19:15:03 +0900

    feat: add default dragover eventHandler for immediate dragend event

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import GlobalStyle from './styles/global';
 import TaskMachine from './pages/TaskMachine';
 
 function App() {
+  /**
+   * 아래 이벤트 핸들러를 넣어주어야 dragend 이벤트가 즉각 발생한다.
+   */
+  document.addEventListener("dragover", function( event ) {
+    event.preventDefault();
+  }, false);
+
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
